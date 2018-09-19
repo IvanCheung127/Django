4c578212913ed482ecd62b5f558da7432197ca62
ForkZ <402454521@qq.com>
2018-09-19 17:12:16 +0800

    initial

---
+++ blog/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+import blog.views
+
+urlpatterns = [
+    path('', blog.views.archive),]
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render_to_response
+from blog.models import BlogPost
+
+
+def archive(request):
+    posts = BlogPost.objects.all()
+    return render_to_response('archive.html', {'posts': posts})
+
+
